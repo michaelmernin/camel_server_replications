0f523e070661defdce8437542447cd6787d1d795
michaelmernin <michael.mernin@gmail.com>
2019-10-21 12:10:10 -0500

    commenting

--- spring_boot_routes/src/main/java/com/java_server_replication_routes/replication/Routes/Rest.java
+++ spring_boot_routes/src/main/java/com/java_server_replication_routes/replication/Routes/Rest.java
@@ -8,23 +8,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class Rest extends RouteBuilder {
 
-    public void configure() {
-        CamelContext context = new DefaultCamelContext();
-
-        rest("/api/")
-                .id("api-route")
-                .consumes("application/json")
-                .post("/bean")
-                .bindingMode(RestBindingMode.json_xml)
-                .type(MyBean.class)
-                .to("direct:remoteService");
-        from("direct:remoteService")
-                .routeId("direct-route")
-                .tracing()
-                .log(">>> ${body.id}")
-                .log(">>> ${body.name}")
-                //.log(String.valueOf(date.getTime()))
-                .transform().simple("Hello ${in.body.name}")
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+   public void configure() {
+//        CamelContext context = new DefaultCamelContext();
+//
+//        rest("/api/")
+//                .id("api-route")
+//                .consumes("application/json")
+//                .post("/bean")
+//                .bindingMode(RestBindingMode.json_xml)
+//                .type(MyBean.class)
+//                .to("direct:remoteService");
+//        from("direct:remoteService")
+//                .routeId("direct-route")
+//                .tracing()
+//                .log(">>> ${body.id}")
+//                .log(">>> ${body.name}")
+//                //.log(String.valueOf(date.getTime()))
+//                .transform().simple("Hello ${in.body.name}")
+//                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
     }
 }
